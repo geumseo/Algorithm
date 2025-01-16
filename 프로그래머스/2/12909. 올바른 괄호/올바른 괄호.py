666f33f4755bebe0ec44dadad3d3e72d60ace7b4
byeon geumseo <103231257+geumseo@users.noreply.github.com>
2025-01-16 14:56:39 +0900

    Update 올바른 괄호.py

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,47 +1,19 @@
+
 def solution(s):
-    data_list = []
     
-    for i in range(len(s)):
-        if s[i] == '(':
-            data_list.append(s[i])
-        else:
-            if data_list == []:
-                return False 
-            else: 
-                data_list.pop() 
+    stack = []
+    
+    for i in s:
+        if i == '(':
+            stack.append(i)
             
-    if data_list != []:
+        else:
+            if stack ==[]:
+                return False
+            else:
+                stack.pop()
+    if stack != []:
         return False
     
     return True
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def solution(s):
-    
-#     stack = []
-    
-#     for i in s:
-#         if i == '(':
-#             stack.append(i)
-            
-#         else:
-#             if stack ==[]:
-#                 return False
-#             else:
-#                 stack.pop()
-#     if stack != []:
-#         return False
-    
-#     return True
             
