95aa4ef71af22d5bb77247185078fa3537de1410
byeon geumseo <103231257+geumseo@users.noreply.github.com>
2025-01-17 16:31:08 +0900

    [level 2] Title: 구명보트, Time: 17.73 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,13 @@
+
+def solution(people, limit):
+    count = 0
+    people.sort()
+    left, right = 0, len(people)-1
+    
+    while(left<=right):
+        if people[left] + people[right] <= limit:
+            left += 1
+        right -= 1
+        count += 1
+    return count
+            
